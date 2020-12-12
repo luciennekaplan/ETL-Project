2a979a9b42b9f234de9b50b7917e6a0ec06a4e4c
MarcyVampQueen <savannahcordry@gmail.com>
2020-12-12 13:41:11 -0800

    data cleaned

--- meteor-cleanup.py
+++ meteor-cleanup.py
@@ -4,13 +4,15 @@ import os
 # Import the raw CSV
 path = os.path.join(os.path.dirname(__file__),"meteorite-landings.csv")
 data = pd.read_csv(path)
-print(data.count())
 
 # Filter out the invalid locations
 filter_location = data.dropna()
 filter_location = filter_location.loc[data["GeoLocation"]!="(0.000000, 0.000000)"]
-print(filter_location.count())
 
 # We only want meteors that fell after 1906
-filter_dates = filter_location.loc[filter_location["year"] > 1906]
-print(filter_dates.count())
+filter_dates = filter_location.loc[filter_location["year"] >= 1906]
+filter_dates = filter_dates.loc[filter_dates["year"] <= 2013]
+
+# Remove lat/long since we have GeoLoc
+meteors = filter_dates[["name","id","nametype","recclass","mass","fall","year","GeoLocation"]]
+print(meteors.head())
